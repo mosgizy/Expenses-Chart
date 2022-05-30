a015bafdda595996e458ee869f03fb9408ca7c3e
mosgizy <moshood521@gmail.com>
2022-05-30 04:02:58 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,59 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const datas = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
 
-import confetti from 'canvas-confetti';
+const stats = document.querySelector(".stats");
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const create = (height: number, day: string): string => {
+  return `
+    <div class="block">
+            <span class="top">$${height}</span>
+            <div class="bar" data-height=${height}></div>
+            <span>${day}</span>
+          </div>
+  `;
+};
+
+datas.forEach((data) => {
+  stats.innerHTML += create(data.amount, data.day);
+});
+
+const bars = document.querySelectorAll(".bar");
+bars.forEach((bar) => {
+  const height: number = bar.dataset.height;
+  bar.style.height = height * 2.5 + "px";
+  bar.addEventListener("mouseover", () => {
+    bar.previousElementSibling.style.display = "block";
+  });
+
+  bar.addEventListener("mouseout", () => {
+    bar.previousElementSibling.style.display = "none";
+  });
+});
