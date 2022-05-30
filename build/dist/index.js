9d6f0ca52e0625e29c1a2d66302ce56c50c4f045
mosgizy <moshood521@gmail.com>
2022-05-30 04:54:54 +0100

    first commit

--- build/dist/index.js
+++ build/dist/index.js
@@ -1,37 +1,35 @@
 const datas = [
   {
     day: "mon",
-    amount: 17.45,
+    amount: 17.45
   },
   {
     day: "tue",
-    amount: 34.91,
+    amount: 34.91
   },
   {
     day: "wed",
-    amount: 52.36,
+    amount: 52.36
   },
   {
     day: "thu",
-    amount: 31.07,
+    amount: 31.07
   },
   {
     day: "fri",
-    amount: 23.39,
+    amount: 23.39
   },
   {
     day: "sat",
-    amount: 43.28,
+    amount: 43.28
   },
   {
     day: "sun",
-    amount: 25.48,
-  },
+    amount: 25.48
+  }
 ];
-
 const stats = document.querySelector(".stats");
-
-const create = (height: number, day: string): string => {
+const create = (height, day) => {
   return `
     <div class="block">
             <span class="top">$${height}</span>
@@ -40,19 +38,16 @@ const create = (height: number, day: string): string => {
           </div>
   `;
 };
-
 datas.forEach((data) => {
   stats.innerHTML += create(data.amount, data.day);
 });
-
 const bars = document.querySelectorAll(".bar");
 bars.forEach((bar) => {
-  const height: number = bar.dataset.height;
+  const height = bar.dataset.height;
   bar.style.height = height * 4 + "px";
   bar.addEventListener("mouseover", () => {
     bar.previousElementSibling.style.display = "block";
   });
-
   bar.addEventListener("mouseout", () => {
     bar.previousElementSibling.style.display = "none";
   });
